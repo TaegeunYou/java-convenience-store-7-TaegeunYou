dce7efb323692518da53d198f5ee0e9f4603aaf0
TaegeunYou <dbxorms2310@naver.com>
2024-11-06 17:07:27 +0900

    test(ReadFileTest): 함수명 변경과 @DisplayName 추가

--- src/test/java/store/global/ReadFileTest.java
+++ src/test/java/store/global/ReadFileTest.java
@@ -2,15 +2,16 @@ package store.global;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.global.constants.FilePath;
-import store.global.file.ReadFile;
 import store.global.file.ReadProductFile;
 
 class ReadFileTest {
 
+    @DisplayName("파일을 읽어서 모든 줄을 정상적으로 가져오는지 확인한다.")
     @Test
-    void getAllLines() {
+    void getAllLinesTest() {
         // given
         ReadProductFile readProductFile = new ReadProductFile();
 
